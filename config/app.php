6cbe8459c26085ee16f481d70784fa1e145c2238
marcofarnezi <marcofarnezi@gmail.com>
2016-10-04 13:14:01 -0300

    Alterado nome space padrão de app para CodeProject

--- config/app.php
+++ config/app.php
@@ -172,11 +172,11 @@ return [
         /*
          * Application Service Providers...
          */
-        App\Providers\AppServiceProvider::class,
-        App\Providers\AuthServiceProvider::class,
-        // App\Providers\BroadcastServiceProvider::class,
-        App\Providers\EventServiceProvider::class,
-        App\Providers\RouteServiceProvider::class,
+        CodeProject\Providers\AppServiceProvider::class,
+        CodeProject\Providers\AuthServiceProvider::class,
+        // CodeProject\Providers\BroadcastServiceProvider::class,
+        CodeProject\Providers\EventServiceProvider::class,
+        CodeProject\Providers\RouteServiceProvider::class,
 
     ],
 
