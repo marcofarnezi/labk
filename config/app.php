1cf7dad1577370ac33e23b81046da5d0d69b152f
marcofarnezi <marcofarnezi@gmail.com>
2016-10-20 13:10:59 -0200

    Configurando OAuth

--- config/app.php
+++ config/app.php
@@ -164,6 +164,9 @@ return [
         Illuminate\View\ViewServiceProvider::class,
 
         Prettus\Repository\Providers\RepositoryServiceProvider::class,
+
+        LucaDegasperi\OAuth2Server\Storage\FluentStorageServiceProvider::class,
+        LucaDegasperi\OAuth2Server\OAuth2ServerServiceProvider::class,
         /*
          * Package Service Providers...
          */
@@ -198,7 +201,7 @@ return [
         'App' => Illuminate\Support\Facades\App::class,
         'Artisan' => Illuminate\Support\Facades\Artisan::class,
         'Auth' => Illuminate\Support\Facades\Auth::class,
-        'Blade' => Illuminate\Support\Facades\Blade::class,
+        'Blade' => Illuminate\Support\Facades\Blade::class,'Authorizer' => LucaDegasperi\OAuth2Server\Facades\Authorizer::class,
         'Bus' => Illuminate\Support\Facades\Bus::class,
         'Cache' => Illuminate\Support\Facades\Cache::class,
         'Config' => Illuminate\Support\Facades\Config::class,
@@ -227,7 +230,7 @@ return [
         'URL' => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
-
+        'Authorizer' => LucaDegasperi\OAuth2Server\Facades\Authorizer::class,
     ],
 
 ];
